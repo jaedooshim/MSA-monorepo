cbddc56113b135386e053f123d8aa7c5fea8d16b
shimjaedoo <woen1637@naver.com>
2024-04-01 01:44:59 +0900

    feat : tcp exception filter

---
+++ apps/main/src/msa.exception.global.filter.ts
@@ -0,0 +1,32 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Response } from 'express';
+
+// 타입추론이 안되어서 인터페이스 생성
+interface ICustomException {
+  response?: {
+    message?: string;
+    error?: string;
+    statusCode?: number;
+  };
+}
+
+@Catch()
+export class MSAExceptionFilter implements ExceptionFilter {
+  catch(exception: ICustomException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status = exception?.response?.statusCode;
+    const errorBody = exception?.response;
+
+    if (status && errorBody) {
+      return response.status(status).json(errorBody);
+    } else {
+      return response.status(500).json({
+        message: '알 수 없는 오류가 발생하였습니다.',
+        error: 'Internal Server',
+        statusCode: 500,
+      });
+    }
+  }
+}
