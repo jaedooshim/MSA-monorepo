b99f05e98b2b6309ae3d1fe7aa3ebf4ed15021b2
shimjaedoo <woen1637@naver.com>
2024-04-03 00:26:18 +0900

    feat : member create dto

---
+++ apps/member/src/types/create/request.dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateMemberDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MinLength(10)
+  @MaxLength(20)
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: '비밀번호는 최소 8~16자로 설정해야합니다.' })
+  @MaxLength(16, { message: '비밀번호는 최소 8~16자로 설정해야합니다.' })
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  country: string;
+}
