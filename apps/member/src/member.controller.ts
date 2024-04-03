c9e49f3af42d78043cf36027893751539f95baaf
shimjaedoo <woen1637@naver.com>
2024-04-03 23:52:05 +0900

    fix : API 경로 => MSA-pattern 변경

--- apps/member/src/member.controller.ts
+++ apps/member/src/member.controller.ts
@@ -1,42 +1,43 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Put, Query } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { CreateMemberDto } from './types/create/request.dto';
 import { MemberParamDto, UpdateMemberDto, UpdatePasswordDto } from './types/update/request.dto';
 import { MemberFindManyDto } from './types/find-many/request.dto';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('member')
 export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 
-  @Post()
+  @MessagePattern('create_member')
   async create(@Body() body: CreateMemberDto): Promise<string> {
     return await this.memberService.create(body);
   }
 
-  @Put('/:id')
+  @MessagePattern('update_own_member')
   // Guard 추가 예정
-  async update(@Body() body: UpdateMemberDto, @Param() param: MemberParamDto): Promise<string> {
+  async updateOwn(@Body() body: UpdateMemberDto, @Param() param: MemberParamDto): Promise<string> {
     return await this.memberService.update(param.id, body);
   }
 
-  @Patch('/:id')
+  @MessagePattern('update_own_password')
   // Guard 추가 예정
-  async updatePassword(@Body() body: UpdatePasswordDto, @Param() param: MemberParamDto): Promise<string> {
+  async updateOwnPassword(@Body() body: UpdatePasswordDto, @Param() param: MemberParamDto): Promise<string> {
     return await this.memberService.updatePassword(param.id, body.oldPassword, body.newPassword);
   }
 
-  @Delete('/:id')
+  @MessagePattern('delete_own_member')
   // Guard 추가 예정
-  async delete(@Param() param: MemberParamDto): Promise<string> {
+  async deleteOwn(@Param() param: MemberParamDto): Promise<string> {
     return await this.memberService.softDelete(param.id);
   }
 
-  @Get('/:id')
+  @MessagePattern('find_unique_member')
   async findUnique(@Param() param: MemberParamDto) {
     return await this.memberService.findUnique(param.id);
   }
 
-  @Get()
+  @MessagePattern('find_many_member')
   async findMany(@Query() query: MemberFindManyDto) {
     return await this.memberService.findMany(query);
   }
