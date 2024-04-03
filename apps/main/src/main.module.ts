4719006966a368e019128c3f953de52c3365f9b7
shimjaedoo <woen1637@naver.com>
2024-04-03 23:37:54 +0900

    chore : [Main Server]member module 추가

--- apps/main/src/main.module.ts
+++ apps/main/src/main.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MainController } from './main.controller';
 import { MainService } from './main.service';
+import { MembersModule } from './members/members.module';
 
 @Module({
-  imports: [],
+  imports: [MembersModule],
   controllers: [MainController],
   providers: [MainService],
 })
