d816b1e947a3f24657641a07bddf4ccd75dfe505
shimjaedoo <woen1637@naver.com>
2024-04-11 20:51:00 +0900

    Test : 기능 test

--- apps/member/src/member.controller.ts
+++ apps/member/src/member.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Query } from '@nestjs/common';
+import { Body, Controller, Param, Query, Post, Put, Patch, Delete, Get } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { CreateMemberDto } from './types/create/request.dto';
 import { MemberParamDto, UpdateMemberDto, UpdatePasswordDto } from './types/update/request.dto';
@@ -9,30 +9,34 @@ import { MessagePattern } from '@nestjs/microservices';
 export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 
-  @MessagePattern('create_member')
+  @Post()
   async create(@Body() body: CreateMemberDto): Promise<string> {
     return await this.memberService.create(body);
   }
 
-  @MessagePattern('update_own_member')
+  // @MessagePattern('update_own_member')
   // Guard 추가 예정
+  @Put(':id')
   async updateOwn(@Body() body: UpdateMemberDto, @Param() param: MemberParamDto): Promise<string> {
     return await this.memberService.update(param.id, body);
   }
 
-  @MessagePattern('update_own_password')
+  // @MessagePattern('update_own_password')
   // Guard 추가 예정
+  @Patch(':id')
   async updateOwnPassword(@Body() body: UpdatePasswordDto, @Param() param: MemberParamDto): Promise<string> {
     return await this.memberService.updatePassword(param.id, body.oldPassword, body.newPassword);
   }
 
-  @MessagePattern('delete_own_member')
+  // @MessagePattern('delete_own_member')
   // Guard 추가 예정
+  @Delete(':id')
   async deleteOwn(@Param() param: MemberParamDto): Promise<string> {
     return await this.memberService.softDelete(param.id);
   }
 
-  @MessagePattern('find_unique_member')
+  // @MessagePattern('find_unique_member')
+  @Get(':id')
   async findUnique(@Param() param: MemberParamDto) {
     return await this.memberService.findUnique(param.id);
   }
