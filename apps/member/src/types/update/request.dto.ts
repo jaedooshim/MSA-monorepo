5e76f9b3187f9b045dd113ac53a755fa80d069f4
shimjaedoo <woen1637@naver.com>
2024-04-11 20:53:59 +0900

    fix : schema 수정에 따른 필드 추가

--- apps/member/src/types/update/request.dto.ts
+++ apps/member/src/types/update/request.dto.ts
@@ -1,4 +1,5 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, Length, MaxLength, MinLength } from 'class-validator';
+import { memberRole } from '@prisma/client';
 
 export class UpdateMemberDto {
   @IsOptional()
@@ -14,16 +15,33 @@ export class UpdateMemberDto {
 
   @IsOptional()
   @IsString()
-  country?: string;
+  @Length(13, 13)
+  phoneNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  subAddress?: string;
+
+  @IsOptional()
+  @IsEnum(memberRole)
+  role?: memberRole;
 }
 
 export class UpdatePasswordDto {
   @IsNotEmpty()
   @IsString()
+  @MinLength(8, { message: '비밀번호는 최소 8~16자로 설정해야합니다.' })
+  @MaxLength(16, { message: '비밀번호는 최소 8~16자로 설정해야합니다.' })
   oldPassword: string;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(8, { message: '비밀번호는 최소 8~16자로 설정해야합니다.' })
+  @MaxLength(16, { message: '비밀번호는 최소 8~16자로 설정해야합니다.' })
   newPassword: string;
 }
 
