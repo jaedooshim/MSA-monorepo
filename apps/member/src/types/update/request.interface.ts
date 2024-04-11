48707b46362bc6367e7bd23f9fb8bae88a0f6ed5
shimjaedoo <woen1637@naver.com>
2024-04-11 20:54:46 +0900

    fix : schema 수정에 따른 필드 추가

--- apps/member/src/types/update/request.interface.ts
+++ apps/member/src/types/update/request.interface.ts
@@ -1,6 +1,11 @@
+import { $Enums } from '@prisma/client';
+
 export interface IMemberUpdate {
   name?: string;
   email?: string;
-  country?: string;
+  phoneNumber?: string;
+  address?: string;
+  subAddress?: string;
+  role?: $Enums.memberRole;
   id?: string;
 }
