32ff59d25d98e221d67a98bd65d06129a559c585
shimjaedoo <woen1637@naver.com>
2024-04-16 17:40:01 +0900

    feat : jwt module

---
+++ libs/jwt/src/jwt.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { sign, verify } from 'jsonwebtoken';
+import { Member } from '@prisma/client';
+import { IPayload } from '@app/jwt/types/payload.interface';
+@Injectable()
+export class JwtService {
+  constructor(private configService: ConfigService) {}
+
+  sign(member: Member): string {
+    const { id, email, role } = member;
+    return sign({ id, email, role }, this.configService.get<string>('ACCESS_TOKEN_SECRET_KEY')!, {
+      expiresIn: this.configService.get<string>('ACCESS_TOKEN_EXPIRES_IN')!,
+    });
+  }
+
+  verify(accessToken: string): IPayload | string {
+    try {
+      return verify(accessToken, this.configService.get<string>('ACCESS_TOKEN_SECRET_KEY')!) as IPayload;
+    } catch (error) {
+      return error.message as string;
+    }
+  }
+}
