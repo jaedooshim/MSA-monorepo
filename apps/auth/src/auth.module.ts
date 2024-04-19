a67fac1ee5ffe65edbe3aef3a1ad381b5263aec1
shimjaedoo <woen1637@naver.com>
2024-04-19 20:19:20 +0900

    chore : [auth-server] admin module 추가

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthService } from './auth.service';
 import { BcryptModule } from '@app/bcrypt';
 import { MemberModule } from '../../member/src/member.module';
 import { JwtModule } from '@app/jwt';
+import { AdminModule } from '../../admin/src/admin.module';
 
 @Module({
-  imports: [BcryptModule, MemberModule, JwtModule],
+  imports: [BcryptModule, MemberModule, JwtModule, AdminModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
