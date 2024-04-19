94e2529e65c41a4c02edfebc330da39f9d1d954d
shimjaedoo <woen1637@naver.com>
2024-04-19 20:19:45 +0900

    chore : [category-server] category API

---
+++ apps/category/src/category.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CategoryCreate } from './types/create/request.dto';
+import { AdminAuthGuard } from '@app/guard/admin.auth.guard';
+import { CategoryParamDto, CategoryUpdateDto } from './types/update/request.dto';
+import { Category } from '@prisma/client';
+import { FindManyDto } from './types/find-many/request.dto';
+
+@Controller('category')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post()
+  @UseGuards(AdminAuthGuard)
+  async create(@Body() body: CategoryCreate): Promise<string> {
+    return await this.categoryService.create(body);
+  }
+
+  @Patch(':id')
+  @UseGuards(AdminAuthGuard)
+  async update(@Body() body: CategoryUpdateDto, @Param() param: CategoryParamDto): Promise<string> {
+    return await this.categoryService.update(param.id, body);
+  }
+
+  @Delete(':id')
+  @UseGuards(AdminAuthGuard)
+  async delete(@Param() param: CategoryParamDto): Promise<string> {
+    return await this.categoryService.softDelete(param.id);
+  }
+
+  @Get(':id')
+  async findUnique(@Param() param: CategoryParamDto): Promise<Category> {
+    return await this.categoryService.findUnique(param.id);
+  }
+
+  @Get()
+  async findMany(@Query() query: FindManyDto) {
+    return this.categoryService.findMany(query);
+  }
+}
