b7e02219d8deabec11c32cc051138e5b0875c374
shimjaedoo <woen1637@naver.com>
2024-04-19 20:22:40 +0900

    chore : [admin-server] class-transformer 형변환 추가

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters();
   await app.listen(3004);
 }
 bootstrap();
