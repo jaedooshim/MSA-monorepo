5b5d9f3c08132a5ea5159ac0af5f88c80426dd52
shimjaedoo <woen1637@naver.com>
2024-04-19 20:16:40 +0900

    feat : [category-server] 관리자전용 가드

--- libs/guard/src/admin.auth.guard.ts
+++ libs/guard/src/admin.auth.guard.ts
@@ -1,1 +1,27 @@
-// Admin-Guard 작업 예정
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@app/jwt';
+import { Observable } from 'rxjs';
+import { IRequest } from '@app/jwt/types/payload.interface';
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request: IRequest = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  async validateRequest(req: IRequest): Promise<boolean> {
+    let accessToken = req.headers.authorization;
+    if (!accessToken) return false;
+    accessToken = accessToken.split(' ')[1];
+    console.log('accessToken', accessToken);
+
+    const payload = this.jwtService.adminVerify(accessToken);
+    if (typeof payload === 'string') throw new UnauthorizedException('해당하는 권한이 없습니다.');
+
+    req.admin = payload;
+    return true;
+  }
+}
