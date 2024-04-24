69daaf7e7046054ff8c49a8fa5b964b8fa69866f
shimjaedoo <woen1637@naver.com>
2024-04-24 23:48:50 +0900

    chore : [order-server] prisma module import

--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { PrismaModule } from '../../../libs/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
