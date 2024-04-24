1118d5e6c14dca59e821f06b916ce4a0db03f3f0
shimjaedoo <woen1637@naver.com>
2024-04-25 03:25:31 +0900

    feat : [order-server] order update dto

---
+++ apps/order/src/types/update/request.dto.ts
@@ -0,0 +1,20 @@
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { OrderStatus } from '@prisma/client';
+
+export class OrderUpdateDto {
+  @IsOptional()
+  @IsEnum(OrderStatus)
+  orderStatus?: OrderStatus;
+
+  @IsOptional()
+  @IsNumber()
+  productId?: number;
+
+  @IsOptional()
+  @IsString()
+  memberId?: string;
+
+  @IsOptional()
+  @IsString()
+  authKey?: string;
+}
