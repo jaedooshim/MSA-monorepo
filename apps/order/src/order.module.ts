508fcab3490bb3e229600d8eb51ef81ca4500f8b
shimjaedoo <woen1637@naver.com>
2024-04-25 02:56:37 +0900

    feat : [order-server] order module

--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { PrismaModule } from '../../../libs/prisma/prisma.module';
+import { OrderRepository } from './order.repository';
+import { ProductModule } from '../../product/src/product.module';
+import { JwtModule } from '@app/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProductModule, JwtModule],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, OrderRepository],
 })
 export class OrderModule {}
