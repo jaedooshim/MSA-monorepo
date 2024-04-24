6229be4ce0b0ed5a027dbe245f40ad0f46aad943
shimjaedoo <woen1637@naver.com>
2024-04-25 02:56:32 +0900

    feat : [order-server] order module

--- apps/order/src/order.controller.ts
+++ apps/order/src/order.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
+import { OrderCreateDto } from './types/create/request.dto';
+import { NonMemberGuard } from '@app/guard/non-member.guard';
+import { Member } from '@app/decorators/member.decorator';
 
-@Controller()
+@Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
-  @Get()
-  getHello(): string {
-    return this.orderService.getHello();
+  @Post()
+  @UseGuards(NonMemberGuard)
+  async create(@Body() body: OrderCreateDto, @Member() member?): Promise<string | { message: string; authCode?: string }> {
+    const data = { ...body, memberId: member?.id };
+    return await this.orderService.create(data);
   }
 }
