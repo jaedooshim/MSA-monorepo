3b0c4b5ab57f9d84b07685b6ca383ab854c3f21a
shimjaedoo <woen1637@naver.com>
2024-04-25 02:56:49 +0900

    feat : [order-server] order module

--- apps/order/src/order.service.ts
+++ apps/order/src/order.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { OrderRepository } from './order.repository';
+import { IOrderCreate } from './types/create/request.interface';
+import { ProductService } from '../../product/src/product.service';
 
 @Injectable()
 export class OrderService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private orderRepository: OrderRepository,
+    private productService: ProductService,
+  ) {}
+
+  async create(data: IOrderCreate): Promise<string | { message: string; authCode?: string }> {
+    await this.productService.findUnique(data.productId);
+    if (!data.memberId) {
+      const authNumber = await this.orderRepository.authCode();
+      data.authKey = authNumber.toString();
+
+      await this.orderRepository.create(data);
+      return { message: '주문이 성공적으로 요청되었습니다.', authCode: authNumber.toString() };
+    } else {
+      await this.orderRepository.create(data);
+      return '주문이 성공적으로 요청되었습니다.';
+    }
   }
 }
