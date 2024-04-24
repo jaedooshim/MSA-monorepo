4a637a8bea07fc2fbf9413febfdb07cb272dba83
shimjaedoo <woen1637@naver.com>
2024-04-25 02:56:43 +0900

    feat : [order-server] order module

--- apps/order/src/order.repository.ts
+++ apps/order/src/order.repository.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../libs/prisma/prisma.service';
+import { IOrderCreate } from './types/create/request.interface';
+import { Order } from '@prisma/client';
+import { IOrderUpdate } from './types/update/request.interface';
 
 @Injectable()
 export class OrderRepository {
   constructor(private prisma: PrismaService) {}
 
   private orderRepository = this.prisma.extendedClient.order;
+
+  async create(data: IOrderCreate): Promise<Order> {
+    return await this.orderRepository.create({ data });
+  }
+
+  async update(id: number, data: IOrderUpdate): Promise<Order> {
+    return await this.orderRepository.update({ where: { id }, data });
+  }
+  async authCode() {
+    return Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
+  }
 }
