3ca51f422b67d0d8616be85e5b576dcd7bc8ff75
shimjaedoo <woen1637@naver.com>
2024-04-27 03:24:04 +0900

    chore : [order-server] 타입형변환 trans-former 추가

--- apps/order/src/main.ts
+++ apps/order/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { OrderModule } from './order.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrderModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters();
   await app.listen(3007);
 }
 bootstrap();
