7ce705f2dd3c83cc7bc3e9ad29fad4c11d57b70c
shimjaedoo <woen1637@naver.com>
2024-04-26 23:55:29 +0900

    feat : [order-server] order model

--- apps/order/src/order.controller.ts
+++ apps/order/src/order.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderCreateDto } from './types/create/request.dto';
 import { NonMemberGuard } from '@app/guard/non-member.guard';
 import { Member } from '@app/decorators/member.decorator';
+import { OrderParamDto, OrderUpdateDto } from './types/update/request.dto';
 
 @Controller('orders')
 export class OrderController {
@@ -13,5 +14,12 @@ export class OrderController {
   async create(@Body() body: OrderCreateDto, @Member() member?): Promise<string | { message: string; authCode?: string }> {
     const data = { ...body, memberId: member?.id };
     return await this.orderService.create(data);
+  }
+
+  @Put(':id')
+  @UseGuards(NonMemberGuard)
+  async update(@Body() body: OrderUpdateDto, @Param() param: OrderParamDto, @Member() member?): Promise<string> {
+    const data = { ...body, memberId: member?.id };
+    return await this.orderService.update(param.id, data);
   }
 }
