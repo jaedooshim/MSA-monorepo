e281403195c7c7d50a8d10a0394e76cead0b7571
shimjaedoo <woen1637@naver.com>
2024-04-26 23:47:44 +0900

    feat : [order-server] order module

--- apps/order/src/order.service.ts
+++ apps/order/src/order.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { OrderRepository } from './order.repository';
 import { IOrderCreate } from './types/create/request.interface';
 import { ProductService } from '../../product/src/product.service';
+import { IOrderUpdate } from './types/update/request.interface';
 
 @Injectable()
 export class OrderService {
@@ -18,11 +19,20 @@ export class OrderService {
       data.authKey = authNumber.toString();
 
       await this.orderRepository.create(data);
-      return { message: '주문이 성공적으로 요청되었습니다.', authCode: authNumber.toString() };
+      return { message: '주문이 성공적으로 요청되었습니다. \n 다음 서비스를 위해 아래의 시리얼번호를 저장해주세요.', authCode: authNumber.toString() };
       // 멤버 반환값
     } else {
       await this.orderRepository.create(data);
       return '주문이 성공적으로 요청되었습니다.';
     }
+  }
+
+  async update(id: number, data: IOrderUpdate): Promise<string> {
+    await this.productService.findUnique(data.productId);
+    await this.orderRepository.findUniqueOrThrow(id);
+    const nonMember = await this.orderRepository.findUniqueAuthCode(data.authKey);
+    if (data.authKey !== nonMember.authKey) throw new ConflictException('시리얼번호가 일치하지 않습니다. \n 다시 한번 확인해주세요.');
+    await this.orderRepository.update(id, data);
+    return '주문건이 수정되었습니다.';
   }
 }
