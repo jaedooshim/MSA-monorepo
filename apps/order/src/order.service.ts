b4093d87187adab9982ed558a32d3bfb4d10032c
shimjaedoo <woen1637@naver.com>
2024-04-26 23:15:21 +0900

    Comment : [order-server] 주석추가

--- apps/order/src/order.service.ts
+++ apps/order/src/order.service.ts
@@ -12,12 +12,14 @@ export class OrderService {
 
   async create(data: IOrderCreate): Promise<string | { message: string; authCode?: string }> {
     await this.productService.findUnique(data.productId);
+    // 비회원 반환값
     if (!data.memberId) {
       const authNumber = await this.orderRepository.authCode();
       data.authKey = authNumber.toString();
 
       await this.orderRepository.create(data);
       return { message: '주문이 성공적으로 요청되었습니다.', authCode: authNumber.toString() };
+      // 멤버 반환값
     } else {
       await this.orderRepository.create(data);
       return '주문이 성공적으로 요청되었습니다.';
