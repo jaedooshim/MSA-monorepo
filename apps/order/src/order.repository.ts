2a2645506e9aa19e33cc7a87e83959f5b0268e7e
shimjaedoo <woen1637@naver.com>
2024-04-26 23:47:48 +0900

    feat : [order-server] order module

--- apps/order/src/order.repository.ts
+++ apps/order/src/order.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../../libs/prisma/prisma.service';
 import { IOrderCreate } from './types/create/request.interface';
 import { Order } from '@prisma/client';
@@ -21,5 +21,17 @@ export class OrderRepository {
   // 비회원전용 authCode
   async authCode() {
     return Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
+  }
+
+  async findUniqueAuthCode(authKey: string) {
+    const authCode = await this.orderRepository.findUnique({ where: { authKey } });
+    if (!authCode) throw new ConflictException('존재하지 않는 시리얼번호입니다.');
+    return authCode;
+  }
+
+  async findUniqueOrThrow(id: number): Promise<Order> {
+    const order = await this.orderRepository.findFirst({ where: { id } });
+    if (!order) throw new NotFoundException('존재하지 않는 주문번호입니다.');
+    return order;
   }
 }
