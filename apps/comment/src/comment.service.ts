816b346f3d367d9fccfb00a9315634e775aed18b
shimjaedoo <woen1637@naver.com>
2024-04-29 00:11:05 +0900

    feat : [comment-server] 판매자 댓글작성 API 추가

--- apps/comment/src/comment.service.ts
+++ apps/comment/src/comment.service.ts
@@ -6,8 +6,15 @@ import { ICommentCreate } from './types/create/request.interface';
 export class CommentService {
   constructor(private commentRepository: CommentRepository) {}
 
+  // 회원 댓글생성
   async create(data: ICommentCreate): Promise<string> {
     await this.commentRepository.create(data);
+    return '댓글이 성공적으로 작성되었습니다.';
+  }
+
+  // 판매자 댓글생성
+  async salesCreate(data: ICommentCreate): Promise<string> {
+    await this.commentRepository.salesCreate(data);
     return '댓글이 성공적으로 작성되었습니다.';
   }
 }
