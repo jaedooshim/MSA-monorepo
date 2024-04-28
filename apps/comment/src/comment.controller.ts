5d8d7930b0bb74ca2d7eb45097a290fbef2e4739
shimjaedoo <woen1637@naver.com>
2024-04-29 02:34:45 +0900

    feat : [comment-server] comment module

--- apps/comment/src/comment.controller.ts
+++ apps/comment/src/comment.controller.ts
@@ -1,16 +1,17 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { MemberAuthGuard } from '@app/guard/member.auth.guard';
 import { CommentCreateDto } from './types/create/request.dto';
-import { ProductParamDto } from '../../product/src/types/update/request.dto';
 import { Member } from '@app/decorators/member.decorator';
 import { SalesRoleGuard } from '@app/guard/sales.role.guard';
 import { Sales } from '@app/decorators/sales.decorator';
+import { CommentParamDto, CommentUpdateDto } from './types/update/request.dto';
 
 @Controller('comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  // 회원 댓글작성
   @Post()
   @UseGuards(MemberAuthGuard)
   async create(@Body() body: CommentCreateDto, @Member() member): Promise<string> {
@@ -18,10 +19,17 @@ export class CommentController {
     return await this.commentService.create(data);
   }
 
+  //판매자 댓글작성
   @Post('sales')
   @UseGuards(SalesRoleGuard)
   async salesCreate(@Body() body: CommentCreateDto, @Sales() sales): Promise<string> {
     const data = { ...body, adminId: sales.id };
     return await this.commentService.salesCreate(data);
+  }
+
+  @Patch(':id')
+  @UseGuards(MemberAuthGuard)
+  async update(@Body() body: CommentUpdateDto, @Param() param: CommentParamDto, @Member() member): Promise<string> {
+    return await this.commentService.update(param.id, body, member.id);
   }
 }
