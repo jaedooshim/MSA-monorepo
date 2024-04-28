7027fa3d7e73ac4f5ef597522e72568d26dbb22f
shimjaedoo <woen1637@naver.com>
2024-04-29 02:34:37 +0900

    feat : [comment-server] comment module

--- apps/comment/src/comment.service.ts
+++ apps/comment/src/comment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CommentRepository } from './comment.repository';
 import { ICommentCreate } from './types/create/request.interface';
+import { ICommentUpdate } from './types/update/request.interface';
 
 @Injectable()
 export class CommentService {
@@ -16,5 +17,14 @@ export class CommentService {
   async salesCreate(data: ICommentCreate): Promise<string> {
     await this.commentRepository.salesCreate(data);
     return '댓글이 성공적으로 작성되었습니다.';
+  }
+
+  async update(id: number, data: ICommentUpdate, memberId: string): Promise<string> {
+    const comment = await this.commentRepository.findUniqueOrThrow(id);
+    // console.log(comment.memberId);
+    // console.log(memberId);
+    if (comment.memberId !== memberId) throw new UnauthorizedException('해당 댓글수정에 관한 권한이 없습니다.');
+    await this.commentRepository.update(id, data);
+    return '댓글수정이 완료되었습니다.';
   }
 }
