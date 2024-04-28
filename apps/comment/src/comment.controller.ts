73aeb22644b9c5c15c579142ae4f4675b0f76057
shimjaedoo <woen1637@naver.com>
2024-04-28 23:37:57 +0900

    feat : [comment-server] comment module

--- apps/comment/src/comment.controller.ts
+++ apps/comment/src/comment.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { MemberAuthGuard } from '@app/guard/member.auth.guard';
+import { CommentCreateDto } from './types/create/request.dto';
+import { ProductParamDto } from '../../product/src/types/update/request.dto';
+import { Member } from '@app/decorators/member.decorator';
 
-@Controller()
+@Controller('comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
-  @Get()
-  getHello(): string {
-    return this.commentService.getHello();
+  @Post()
+  @UseGuards(MemberAuthGuard)
+  async create(@Body() body: CommentCreateDto, @Member() member): Promise<string> {
+    const data = { ...body, memberId: member.id };
+    return await this.commentService.create(data);
   }
 }
