d07b8b7aba339a127e515d3363867da5a2ad39d1
shimjaedoo <woen1637@naver.com>
2024-04-29 02:33:47 +0900

    fix : 멤버가드 토큰값 없을 시 / 오류반환 메세지 수정

--- libs/guard/src/member.auth.guard.ts
+++ libs/guard/src/member.auth.guard.ts
@@ -15,13 +15,13 @@ export class MemberAuthGuard implements CanActivate {
   async validateRequest(req: IRequest): Promise<boolean> {
     let accessToken = req.headers.authorization;
     // console.log('accessToken', accessToken);
-    if (!accessToken) return false;
+    if (!accessToken) throw new UnauthorizedException('비회원은 이용하실 수 없는 서비스입니다.');
     accessToken = accessToken.split(' ')[1];
     console.log('accessToken', accessToken);
 
     const payload = this.jwtService.verify(accessToken);
     // console.log('payload', payload);
-    if (typeof payload === 'string') throw new UnauthorizedException('비회원은 이용하실 수 없는 서비스입니다.');
+    if (typeof payload === 'string') throw new UnauthorizedException('권한이 없습니다. 다시 한번 확인해주세요.');
 
     req.member = payload;
     return true;
