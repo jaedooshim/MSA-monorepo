d97c1340ec99c9d430817ef744dc8e714f34ba24
shimjaedoo <woen1637@naver.com>
2024-04-28 23:58:42 +0900

    fix : [comment-server] create-comment interface 수정

--- apps/comment/src/comment.service.ts
+++ apps/comment/src/comment.service.ts
@@ -7,9 +7,6 @@ export class CommentService {
   constructor(private commentRepository: CommentRepository) {}
 
   async create(data: ICommentCreate): Promise<string> {
-    if (!data.memberId) {
-      throw new UnauthorizedException('댓글이용 서비스는 회원전용입니다. \n비회원이신 분들은 비회원전용 게시판을 이용해주세요.');
-    }
     await this.commentRepository.create(data);
     return '댓글이 성공적으로 작성되었습니다.';
   }
