9e9a73e083167b3a5cc7108211f47e96a3f48c9d
shimjaedoo <woen1637@naver.com>
2024-04-28 23:37:52 +0900

    feat : [comment-server] comment module

--- apps/comment/src/comment.module.ts
+++ apps/comment/src/comment.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
+import { CommentRepository } from './comment.repository';
+import { PrismaModule } from '../../../libs/prisma/prisma.module';
+import { ProductModule } from '../../product/src/product.module';
+import { JwtModule } from '@app/jwt';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ProductModule, JwtModule],
   controllers: [CommentController],
-  providers: [CommentService],
+  providers: [CommentService, CommentRepository],
 })
 export class CommentModule {}
