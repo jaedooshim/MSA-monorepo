368c04620baac75569cfb39a0de4dc1ac7e2e3d0
shimjaedoo <woen1637@naver.com>
2024-04-28 23:37:13 +0900

    chore : [comment-server] trans-former 설정

--- apps/comment/src/main.ts
+++ apps/comment/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { CommentModule } from './comment.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(CommentModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters();
   await app.listen(3008);
 }
 bootstrap();
