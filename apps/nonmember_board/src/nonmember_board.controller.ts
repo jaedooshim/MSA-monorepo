f5d5d7b853d05258d2f7619a790a4327085cab1b
shimjaedoo <woen1637@naver.com>
2024-04-30 04:19:01 +0900

    feat : [NonMember_Board-server] nonMemberBoard module

--- apps/nonmember_board/src/nonmember_board.controller.ts
+++ apps/nonmember_board/src/nonmember_board.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { NonmemberBoardService } from './nonmember_board.service';
+import { NonMemberBoardCreateDto } from './types/create/request.dto';
+import { NonMemberBoardParamDto, NonMemberBoardUpdateDto } from './types/update/request.dto';
+import { NonMemberBoardDeleteDto } from './types/delete/request.dto';
+import { NonMemberBoardFindManyDto } from './types/find-many/request.dto';
 
-@Controller()
+@Controller('nonmemberboards')
 export class NonmemberBoardController {
   constructor(private readonly nonmemberBoardService: NonmemberBoardService) {}
 
+  @Post()
+  async create(@Body() body: NonMemberBoardCreateDto, @Body('authKey') authKey: string): Promise<string> {
+    return await this.nonmemberBoardService.create(body, authKey);
+  }
+
+  @Put(':id')
+  async update(@Body() body: NonMemberBoardUpdateDto, @Body('authKey') authKey: string, @Param() param: NonMemberBoardParamDto): Promise<string> {
+    return await this.nonmemberBoardService.update(param.id, body, authKey);
+  }
+
+  @Delete(':id')
+  async delete(@Body() body: NonMemberBoardDeleteDto, @Body('authKey') authKey: string, @Param() param: NonMemberBoardParamDto): Promise<string> {
+    return await this.nonmemberBoardService.softDelete(param.id, body, authKey);
+  }
+
+  @Get(':id')
+  async findUnique(@Param() param: NonMemberBoardParamDto) {
+    return await this.nonmemberBoardService.findUnique(param.id);
+  }
+
   @Get()
-  getHello(): string {
-    return this.nonmemberBoardService.getHello();
+  async findMany(@Query() query: NonMemberBoardFindManyDto) {
+    return await this.nonmemberBoardService.findMany(query);
   }
 }
