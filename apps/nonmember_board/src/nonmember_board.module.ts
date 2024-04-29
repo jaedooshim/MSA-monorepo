e8770c77c79bb675e21baed1fdea72c585d83439
shimjaedoo <woen1637@naver.com>
2024-04-30 04:19:05 +0900

    feat : [NonMember_Board-server] nonMemberBoard module

--- apps/nonmember_board/src/nonmember_board.module.ts
+++ apps/nonmember_board/src/nonmember_board.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { NonmemberBoardController } from './nonmember_board.controller';
 import { NonmemberBoardService } from './nonmember_board.service';
 import { PrismaModule } from '../../../libs/prisma/prisma.module';
+import { NonmemberBoardRepository } from './nonmember_board.repository';
+import { OrderModule } from '../../order/src/order.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, OrderModule],
   controllers: [NonmemberBoardController],
-  providers: [NonmemberBoardService],
+  providers: [NonmemberBoardService, NonmemberBoardRepository],
 })
 export class NonmemberBoardModule {}
