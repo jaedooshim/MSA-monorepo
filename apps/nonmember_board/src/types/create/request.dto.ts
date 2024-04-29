031136beb589cefaef2447d2ff87b56b80eec373
shimjaedoo <woen1637@naver.com>
2024-04-30 04:19:37 +0900

    feat : [NonMember_Board-server] nonMemberBoard create dto

---
+++ apps/nonmember_board/src/types/create/request.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class NonMemberBoardCreateDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  orderId: number;
+}
