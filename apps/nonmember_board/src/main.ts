a2816efa62bac6a55310f60e2c3e2c53559fdf38
shimjaedoo <woen1637@naver.com>
2024-04-30 04:18:33 +0900

    chore : [NonMember_Board-server] ValidationPipe 설정

--- apps/nonmember_board/src/main.ts
+++ apps/nonmember_board/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NonmemberBoardModule } from './nonmember_board.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(NonmemberBoardModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters();
   await app.listen(3009);
 }
 bootstrap();
