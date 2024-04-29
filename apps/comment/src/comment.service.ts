25f08d24f01357aee0934a04b8fc4f375eb47db2
shimjaedoo <woen1637@naver.com>
2024-04-29 23:35:18 +0900

    feat : [comment-server] comment module

--- apps/comment/src/comment.service.ts
+++ apps/comment/src/comment.service.ts
@@ -2,6 +2,8 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CommentRepository } from './comment.repository';
 import { ICommentCreate } from './types/create/request.interface';
 import { ICommentUpdate } from './types/update/request.interface';
+import { Comment } from '@prisma/client';
+import { ICommentFindMany } from './types/find-many/request.interface';
 
 @Injectable()
 export class CommentService {
@@ -34,5 +36,27 @@ export class CommentService {
     if (comment.adminId !== salesId) throw new UnauthorizedException('해당 댓글수정에 관한 권한이 없습니다.');
     await this.commentRepository.salesUpdate(id, data);
     return '댓글수정이 완료되었습니다.';
+  }
+
+  async softDelete(id: number, memberId: string): Promise<string> {
+    const comment = await this.commentRepository.findUniqueOrThrow(id);
+    if (comment.memberId !== memberId) throw new UnauthorizedException('해당 댓글삭제에 관한 권한이 없습니다.');
+    await this.commentRepository.softDelete(id);
+    return '댓글삭제가 완료되었습니다.';
+  }
+
+  async salesSoftDelete(id: number, salesId: string): Promise<string> {
+    const comment = await this.commentRepository.findUniqueOrThrow(id);
+    if (comment.adminId !== salesId) throw new UnauthorizedException('해당 댓글삭제에 관한 권한이 없습니다.');
+    await this.commentRepository.salesSoftDelete(id);
+    return '댓글삭제가 완료되었습니다.';
+  }
+
+  async findUnique(id: number): Promise<Comment> {
+    return await this.commentRepository.findUniqueOrThrow(id);
+  }
+
+  async findMany(data: ICommentFindMany) {
+    return await this.commentRepository.findMany(data);
   }
 }
