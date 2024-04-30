597184351459b537f683ba3ed5b0ccab65cafa92
shimjaedoo <woen1637@naver.com>
2024-04-30 18:47:38 +0900

    fix : [main-server] members module JwtModule import 및 TCP 설정

--- apps/main/src/members/members.module.ts
+++ apps/main/src/members/members.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MembersController } from './members.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { JwtModule } from '@app/jwt';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         options: { host: '127.0.0.1', port: 3002 },
       },
     ]),
+    JwtModule,
   ],
   controllers: [MembersController],
 })
