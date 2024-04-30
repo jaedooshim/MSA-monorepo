6b6f5d20a8949eaa96ad90893f881958c884b1fb
shimjaedoo <woen1637@naver.com>
2024-04-30 12:01:18 +0900

    feat : [main-server] MSA member API

--- apps/main/src/members/members.controller.ts
+++ apps/main/src/members/members.controller.ts
@@ -1,10 +1,46 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { CreateMemberDto } from '../../../member/src/types/create/request.dto';
+import { MemberParamDto, UpdateMemberDto, UpdatePasswordDto } from '../../../member/src/types/update/request.dto';
+import { MemberAuthGuard } from '@app/guard/member.auth.guard';
+import { MemberFindManyDto } from '../../../member/src/types/find-many/request.dto';
 
 @Controller('members')
 export class MembersController {
   constructor(@Inject('MEMBER_SERVICE') private client: ClientProxy) {}
 
-  // @Post()
-  // async create(@Body() body)
+  @Post()
+  async create(@Body() body: CreateMemberDto) {
+    return this.client.send<string>('create_member', { ...body });
+  }
+
+  @Put(':id')
+  // @UseGuards(MemberAuthGuard)
+  async update(@Body() body: UpdateMemberDto, @Param() param: MemberParamDto) {
+    return this.client.send<string>('update_own_member', { id: param.id, body });
+  }
+
+  @Patch(':id/password')
+  // @UseGuards(MemberAuthGuard)
+  async updatePassword(@Body() body: UpdatePasswordDto, @Param() param: MemberParamDto) {
+    console.log('param.id', param.id);
+    console.log('body', body);
+    return this.client.send<string>('update_own_password', { id: param.id, oldPassword: body.oldPassword, newPassword: body.newPassword });
+  }
+
+  @Delete(':id')
+  // @UseGuards(MemberAuthGuard)
+  async delete(@Param() param: MemberParamDto) {
+    return this.client.send<string>('delete_own_member', { id: param.id });
+  }
+
+  @Get(':id')
+  async findUnique(@Param() param: MemberParamDto) {
+    return this.client.send<string>('find_unique_member', { id: param.id });
+  }
+
+  @Get()
+  async findMany(@Query() query: MemberFindManyDto) {
+    return this.client.send<string>('find_many_member', query);
+  }
 }
