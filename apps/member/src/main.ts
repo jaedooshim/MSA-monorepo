8c4d2d9835a5ac003262a89edc05e384fe4b2dcd
shimjaedoo <woen1637@naver.com>
2024-04-30 12:03:06 +0900

    chore : [member-server] MSA tcp 포트 설정

--- apps/member/src/main.ts
+++ apps/member/src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { MemberModule } from './member.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaConfig } from 'libs/prisma/prisma.config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 async function bootstrap() {
-  const app = await NestFactory.create(MemberModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(MemberModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 3002,
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -21,6 +29,6 @@ async function bootstrap() {
     query: true,
     info: true,
   });
-  await app.listen(3002);
+  await app.listen();
 }
 bootstrap();
