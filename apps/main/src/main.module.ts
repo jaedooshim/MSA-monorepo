72c4369e272603625ddfc2513664ff3805b611b1
shimjaedoo <woen1637@naver.com>
2024-04-30 18:43:24 +0900

    chore : [main-server] authModule import

--- apps/main/src/main.module.ts
+++ apps/main/src/main.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MainController } from './main.controller';
 import { MainService } from './main.service';
 import { MembersModule } from './members/members.module';
+import { AuthsModule } from './auths/auths.module';
 
 @Module({
-  imports: [MembersModule],
+  imports: [MembersModule, AuthsModule],
   controllers: [MainController],
   providers: [MainService],
 })
