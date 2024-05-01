082b177c81400345e218d94f994ff9e4a7ac519f
shimjaedoo <woen1637@naver.com>
2024-05-01 14:40:34 +0900

    fix : [auth-server] auth messagePattern 변경

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './types/request.dto';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  async login(@Body() body: LoginDto) {
+  @MessagePattern('member_login')
+  async login(body: LoginDto): Promise<string> {
     return await this.authService.login(body);
   }
 
-  @Post('admin')
-  async adminLogin(@Body() body: LoginDto) {
+  @MessagePattern('admin_login')
+  async adminLogin(body: LoginDto): Promise<string> {
     return await this.authService.adminLogin(body);
   }
 }
