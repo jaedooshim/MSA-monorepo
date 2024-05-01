69def2f4a7ce62f832b59184abeb2039148322e2
shimjaedoo <woen1637@naver.com>
2024-05-01 19:02:40 +0900

    chore : [order-server] MSA TCP 포트설정

--- apps/order/src/main.ts
+++ apps/order/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { OrderModule } from './order.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(OrderModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(OrderModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 3007,
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +22,6 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters();
-  await app.listen(3007);
+  await app.listen();
 }
 bootstrap();
