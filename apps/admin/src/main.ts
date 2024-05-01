18510b4a2db4d633d5718d14ec34b41ac70af7bd
shimjaedoo <woen1637@naver.com>
2024-05-01 15:40:15 +0900

    chore : [admin-server] MSA admin TCP 포트 설정

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AdminModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AdminModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 3004,
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +22,6 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters();
-  await app.listen(3004);
+  await app.listen();
 }
 bootstrap();
