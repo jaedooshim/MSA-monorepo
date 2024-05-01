b6efeefdfce514c65d0cabc4e07df4a73091b242
shimjaedoo <woen1637@naver.com>
2024-05-01 18:15:00 +0900

    chore : [main-server] MSA product TCP 포트설정

---
+++ apps/main/src/products/products.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { JwtModule } from '@app/jwt';
+import { ProductsController } from './products.controller';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.TCP,
+        options: { host: '127.0.0.1', port: 3006 },
+      },
+    ]),
+    JwtModule,
+  ],
+  controllers: [ProductsController],
+})
+export class ProductsModule {}
