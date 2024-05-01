4e2d6d743551bbce990fbea301fc07ec6bab403c
shimjaedoo <woen1637@naver.com>
2024-05-01 15:40:43 +0900

    chore : [main-server] adminsModule 추가

--- apps/main/src/main.module.ts
+++ apps/main/src/main.module.ts
@@ -3,9 +3,10 @@ import { MainController } from './main.controller';
 import { MainService } from './main.service';
 import { MembersModule } from './members/members.module';
 import { AuthsModule } from './auths/auths.module';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
-  imports: [MembersModule, AuthsModule],
+  imports: [MembersModule, AuthsModule, AdminsModule],
   controllers: [MainController],
   providers: [MainService],
 })
