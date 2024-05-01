3c47959464db05e740df64cb7019d324ceb5274b
shimjaedoo <woen1637@naver.com>
2024-05-01 19:02:19 +0900

    chore : [main-server] ordersModule import

--- apps/main/src/main.module.ts
+++ apps/main/src/main.module.ts
@@ -6,9 +6,10 @@ import { AuthsModule } from './auths/auths.module';
 import { AdminsModule } from './admins/admins.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [MembersModule, AuthsModule, AdminsModule, CategoriesModule, ProductsModule],
+  imports: [MembersModule, AuthsModule, AdminsModule, CategoriesModule, ProductsModule, OrdersModule],
   controllers: [MainController],
   providers: [MainService],
 })
