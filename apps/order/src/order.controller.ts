43f21db73818c513a6722c3ddd67375079d0711f
shimjaedoo <woen1637@naver.com>
2024-05-02 15:16:10 +0900

    feat : [order-server] MSA messagePattern 변경 / 타입변경

--- apps/order/src/order.controller.ts
+++ apps/order/src/order.controller.ts
@@ -1,12 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { OrderService } from './order.service';
-import { OrderCreateDto } from './types/create/request.dto';
-import { NonMemberGuard } from '@app/guard/non-member.guard';
-import { Member } from '@app/decorators/member.decorator';
-import { OrderAdminUpdate, OrderParamDto, OrderUpdateDto } from './types/update/request.dto';
-import { SalesRoleGuard } from '@app/guard/sales.role.guard';
-import { Sales } from '@app/decorators/sales.decorator';
-import { MemberAuthGuard } from '@app/guard/member.auth.guard';
+import { OrderAdminUpdate, OrderUpdateDto } from './types/update/request.dto';
 import { OrderDeleteNonMemberDto } from './types/delete/request.dto';
 import { OrderFindManyDto } from './types/find-many/request.dto';
 import { MessagePattern } from '@nestjs/microservices';
@@ -22,37 +16,32 @@ export class OrderController {
   }
 
   @MessagePattern('update_order')
-  async update(@Body() body: OrderUpdateDto, @Param() param: OrderParamDto, @Member() member?): Promise<string> {
-    const data = { ...body, memberId: member?.id };
-    return await this.orderService.update(param.id, data);
+  async update(data: { id: number; body: OrderUpdateDto }): Promise<string> {
+    return await this.orderService.update(data.id, data.body);
   }
 
-  @Patch(':id')
-  @UseGuards(SalesRoleGuard)
-  async adminUpdate(@Body() body: OrderAdminUpdate, @Param() param: OrderParamDto, @Sales() sales): Promise<string> {
-    return await this.orderService.adminUpdate(param.id, body, sales.id);
+  @MessagePattern('update_admin_order')
+  async adminUpdate(data: { id: number; body: OrderAdminUpdate; adminId: string }): Promise<string> {
+    return await this.orderService.adminUpdate(data.id, data.body, data.adminId);
   }
 
-  @Delete(':id')
-  @UseGuards(MemberAuthGuard)
-  async delete(@Param() param: OrderParamDto, @Member() member): Promise<string> {
-    return await this.orderService.softDelete(param.id, member.id);
+  @MessagePattern('delete_member_order')
+  async delete(data: { id: number; memberId: string }): Promise<string> {
+    return await this.orderService.softDelete(data.id, data.memberId);
   }
 
-  @Delete('nonmember/:id')
-  @UseGuards(NonMemberGuard)
-  async deleteNonMember(@Body() body: OrderDeleteNonMemberDto, @Param() param: OrderParamDto): Promise<string> {
-    return await this.orderService.softDeleteNonMember(param.id, body);
+  @MessagePattern('delete_nonMember_order')
+  async deleteNonMember(data: { id: number; body: OrderDeleteNonMemberDto }): Promise<string> {
+    return await this.orderService.softDeleteNonMember(data.id, data.body);
   }
 
-  @Get(':id')
-  @UseGuards(NonMemberGuard)
-  async findUnique(@Param() param: OrderParamDto) {
-    return await this.orderService.findUnique(param.id);
+  @MessagePattern('find_unique_order')
+  async findUnique(id: number) {
+    return await this.orderService.findUnique(id);
   }
 
-  @Get()
-  async findMany(@Query() query: OrderFindManyDto) {
-    return await this.orderService.findMany(query);
+  @MessagePattern('find_many_order')
+  async findMany(data: OrderFindManyDto) {
+    return await this.orderService.findMany(data);
   }
 }
