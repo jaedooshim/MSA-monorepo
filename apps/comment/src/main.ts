591a7013d6c87422f3685da51c5b571b5a389221
shimjaedoo <woen1637@naver.com>
2024-05-02 20:16:31 +0900

    chore: [comment-server] MSA TCP port설정

--- apps/comment/src/main.ts
+++ apps/comment/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { CommentModule } from './comment.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(CommentModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(CommentModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 3008,
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +22,6 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters();
-  await app.listen(3008);
+  await app.listen();
 }
 bootstrap();
