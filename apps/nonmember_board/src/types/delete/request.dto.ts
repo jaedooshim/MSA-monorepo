25aba5957056b2750198de13dc227fedb7a8b467
shimjaedoo <woen1637@naver.com>
2024-05-02 21:36:26 +0900

    Refactor: [nonmember_board-server] delete dto -> authKey 속성추가

--- apps/nonmember_board/src/types/delete/request.dto.ts
+++ apps/nonmember_board/src/types/delete/request.dto.ts
@@ -1,7 +1,11 @@
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class NonMemberBoardDeleteDto {
-  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  authKey: string;
+
+  @IsNotEmpty()
   @IsNumber()
-  orderId?: number;
+  orderId: number;
 }
