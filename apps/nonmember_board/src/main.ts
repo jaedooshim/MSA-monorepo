97641337cb8e55e0a45691c44acd36d5a72741fe
shimjaedoo <woen1637@naver.com>
2024-05-02 21:38:41 +0900

    chore : [nonmember_board-server] MSA TCP 포트설정

--- apps/nonmember_board/src/main.ts
+++ apps/nonmember_board/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { NonmemberBoardModule } from './nonmember_board.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NonmemberBoardModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(NonmemberBoardModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 3009,
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +22,6 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters();
-  await app.listen(3009);
+  await app.listen();
 }
 bootstrap();
