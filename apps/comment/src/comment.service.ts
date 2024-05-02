ae12d03d2f1163f4348289fb9d07bbb2a6dd89b0
shimjaedoo <woen1637@naver.com>
2024-05-02 20:18:16 +0900

    fix: [comment-server] 타입변경

--- apps/comment/src/comment.service.ts
+++ apps/comment/src/comment.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CommentRepository } from './comment.repository';
-import { ICommentCreate } from './types/create/request.interface';
 import { ICommentUpdate } from './types/update/request.interface';
-import { Comment } from '@prisma/client';
+import { Comment, Prisma } from '@prisma/client';
 import { ICommentFindMany } from './types/find-many/request.interface';
 
 @Injectable()
@@ -10,13 +9,13 @@ export class CommentService {
   constructor(private commentRepository: CommentRepository) {}
 
   // 회원 댓글생성
-  async create(data: ICommentCreate): Promise<string> {
+  async create(data: Prisma.CommentUncheckedCreateInput): Promise<string> {
     await this.commentRepository.create(data);
     return '댓글이 성공적으로 작성되었습니다.';
   }
 
   // 판매자 댓글생성
-  async salesCreate(data: ICommentCreate): Promise<string> {
+  async salesCreate(data: Prisma.CommentUncheckedCreateInput): Promise<string> {
     await this.commentRepository.salesCreate(data);
     return '댓글이 성공적으로 작성되었습니다.';
   }
