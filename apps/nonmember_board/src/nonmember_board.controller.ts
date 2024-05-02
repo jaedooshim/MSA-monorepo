b2c45dba597080a4a1f64f0d5b96d38f72884bf7
shimjaedoo <woen1637@naver.com>
2024-05-02 21:41:59 +0900

    Refactor :[nonmember_board-server] MSA messagePattern 변경 / 타입변경

--- apps/nonmember_board/src/nonmember_board.controller.ts
+++ apps/nonmember_board/src/nonmember_board.controller.ts
@@ -1,36 +1,37 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { NonmemberBoardService } from './nonmember_board.service';
 import { NonMemberBoardCreateDto } from './types/create/request.dto';
-import { NonMemberBoardParamDto, NonMemberBoardUpdateDto } from './types/update/request.dto';
+import { NonMemberBoardUpdateDto } from './types/update/request.dto';
 import { NonMemberBoardDeleteDto } from './types/delete/request.dto';
 import { NonMemberBoardFindManyDto } from './types/find-many/request.dto';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('nonmemberboards')
 export class NonmemberBoardController {
   constructor(private readonly nonmemberBoardService: NonmemberBoardService) {}
 
-  @Post()
-  async create(@Body() body: NonMemberBoardCreateDto, @Body('authKey') authKey: string): Promise<string> {
-    return await this.nonmemberBoardService.create(body, authKey);
+  @MessagePattern('create_nonmember_board')
+  async create(body: NonMemberBoardCreateDto): Promise<string> {
+    return await this.nonmemberBoardService.create(body);
   }
 
-  @Put(':id')
-  async update(@Body() body: NonMemberBoardUpdateDto, @Body('authKey') authKey: string, @Param() param: NonMemberBoardParamDto): Promise<string> {
-    return await this.nonmemberBoardService.update(param.id, body, authKey);
+  @MessagePattern('update_nonmember_board')
+  async update(data: { id: number; body: NonMemberBoardUpdateDto }): Promise<string> {
+    return await this.nonmemberBoardService.update(data.id, data.body);
   }
 
-  @Delete(':id')
-  async delete(@Body() body: NonMemberBoardDeleteDto, @Body('authKey') authKey: string, @Param() param: NonMemberBoardParamDto): Promise<string> {
-    return await this.nonmemberBoardService.softDelete(param.id, body, authKey);
+  @MessagePattern('delete_nonmember_board')
+  async delete(data: { id: number; body: NonMemberBoardDeleteDto }): Promise<string> {
+    return await this.nonmemberBoardService.softDelete(data.id, data.body);
   }
 
-  @Get(':id')
-  async findUnique(@Param() param: NonMemberBoardParamDto) {
-    return await this.nonmemberBoardService.findUnique(param.id);
+  @MessagePattern('find_unique_nonmember_board')
+  async findUnique(data: { id: number }) {
+    return await this.nonmemberBoardService.findUnique(data.id);
   }
 
-  @Get()
-  async findMany(@Query() query: NonMemberBoardFindManyDto) {
-    return await this.nonmemberBoardService.findMany(query);
+  @MessagePattern('find_many_nonmember_board')
+  async findMany(data: NonMemberBoardFindManyDto) {
+    return await this.nonmemberBoardService.findMany(data);
   }
 }
